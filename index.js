25916cee0434850b339ce14b5d1c69a87e4c793d
07andre07 <andremontanharussa@gmail.com>
2026-01-15 20:51:32 -0500

    fix: update database connection to use pg.Pool and correct database name

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = new pg.Client({
+const db = new pg.Pool({
   user: "postgres",
   host: "localhost",
-  database: "Bootcamp_AngelaWU",
+  database: "tasks_db",
   password: "Luis2010#4571",
   port: 5432,
 });
@@ -20,11 +20,24 @@ const port = 3000;
 
 db.connect();
 
-
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     res.render('index.ejs')
 });
 
+app.post("/tasks", async (req, res) => {
+    const { title, description } = req.body;
+    
+    try {
+        await db.query(
+            "INSERT INTO tasks (title, description) VALUES ($1, $2)",
+            [title, description]
+        );
+        res.json('Task added successfully, the title is settled as  (' + title + ") and the description as  (" + description + ')');
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to add task' });
+    }
+});
 app.listen(port, () => {
-    console.log('Server running on port ', port)
+    console.log('Server running on port ' + port)
 });
